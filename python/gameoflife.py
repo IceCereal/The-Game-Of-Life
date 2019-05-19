695f75369dea0108137489f4330c1037eca9fefe
IceCereal <ns_raghav@hotmail.com>
2019-05-19 12:04:49 +0530

    create updateGrid and add 4 corners

--- python/gameoflife.py
+++ python/gameoflife.py
@@ -18,3 +18,17 @@ netVals = [[0 for i in range(WIDTH)] for j in range(HEIGHT)]
 # RANGE FOR EACH CELL
 lowerBound = 2
 upperBound = 3
+
+# MAIN LOOP
+def updateGrid():
+	# VERSION = 2.5
+
+	# EASY VALS:
+	maxColumn = WIDTH - 1
+	maxRow = HEIGHT - 1
+
+	# 4 Corners
+	netVals[0][0] = grid[0][1] + grid[1][1] + grid[1][0]
+	netVals[0][maxColumn] = grid[0][maxColumn-1] + grid[1][maxColumn-1] + grid[1][maxColumn]
+	netVals[maxRow][maxColumn] = grid[maxRow][maxColumn-1] + grid[maxRow-1][maxColumn-1] + grid[maxRow-1][maxColumn]
+	netVals[maxRow][0] = grid[maxRow][1] + grid[maxRow-1][1] + grid[maxRow-1][0]
