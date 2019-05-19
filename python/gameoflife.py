084bcfc4d9bc1ff0872c5d54972377267e1e5f2a
IceCereal <ns_raghav@hotmail.com>
2019-05-19 12:03:00 +0530

    add basic vars

--- python/gameoflife.py
+++ python/gameoflife.py
@@ -3,3 +3,18 @@
 	Author: IceCereal
 """
 
+# IMPORTS
+import random
+import pygame
+
+# HEIGHT & WIDTH OF THE GRID
+HEIGHT = 200
+WIDTH = 200
+
+# GRID AND IT'S CORRESPONDING EQUIVALENCY VALUES
+grid = [[0 for i in range(WIDTH)] for j in range(HEIGHT)]
+netVals = [[0 for i in range(WIDTH)] for j in range(HEIGHT)]
+
+# RANGE FOR EACH CELL
+lowerBound = 2
+upperBound = 3
